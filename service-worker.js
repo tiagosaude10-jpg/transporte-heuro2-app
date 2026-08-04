1e1eca3b0ac527b438aa0a999dbb641a12bcb0f7
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-08-03 23:06:02 -0400

    Desativar cache durante a construção e limpar versões antigas

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,17 @@
-const CACHE_NAME = 'transporte-heuro2-v5';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/app.js',
-  './manifest.json',
-  './A5B6B48E-DCCE-4B36-9A5F-F7C1D1225E82.png',
-  './99BD9DA5-6979-422A-99EB-B41F091CC5FE.png',
-  './544AB6B5-F264-4518-A2AE-1A3679A7EB68.png'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
+    Promise.all([
+      caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+      self.registration.unregister(),
+      self.clients.claim()
+    ])
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  const request = event.request;
-  const isNavigation = request.mode === 'navigate';
-  const isCodeFile = /\.(?:html|css|js|json)$/.test(new URL(request.url).pathname);
-
-  if (isNavigation || isCodeFile) {
-    event.respondWith(
-      fetch(request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
-          return response;
-        })
-        .catch(() => caches.match(request).then((cached) => cached || caches.match('./index.html')))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request).then((cached) => {
-      if (cached) return cached;
-      return fetch(request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
-        return response;
-      });
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
