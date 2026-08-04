03d7e50451f14369cf2e0109cf09a0f2b18390d3
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-08-03 22:10:51 -0400

    Adicionar cache básico da primeira versão instalável

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'transporte-heuro2-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './manifest.json',
+  './A5B6B48E-DCCE-4B36-9A5F-F7C1D1225E82.png',
+  './assets/images/tela-inicial.jpeg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      });
+    })
+  );
+});
