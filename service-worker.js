1199db7bf025f93e43dd231b9eb25a119627cf8b
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-08-04 06:17:52 -0400

    Desativar cache durante a construção

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,14 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     Promise.all([
-      caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
-      self.registration.unregister(),
-      self.clients.claim()
+      caches.keys().then((cacheNames) => Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      )),
+      self.registration.unregister()
     ])
   );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
 });
